29fc8b2594e7b5b930b1b0f219437fab9fe9b498
FBVS <fredsoto641@gmail.com>
2026-06-25 13:40:45 -0500

    commit

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://pink-pigeon-455462.hostingersite.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
